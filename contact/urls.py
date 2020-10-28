e5550442475f3a1650d7854db44d8f361ecc6d41
abdykasymovvv <abdykasymov_e@iuca.kg>
2020-10-28 22:02:39 +0600

    My project

--- contact/urls.py
+++ contact/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
    path('', views.contact_home, name='detail'),
    path('create', views.create, name='create'),
    path('<int:pk>', views.ContactDetailView.as_view(), name='news-detail'),
- path('list', views.list_go, name='list')
+    path('list', views.list_go, name='list')
    ]
